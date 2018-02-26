92c6e525de7a2e376342e9a25ffaf1602d74d59d
Nate Mallison <healthynate@gmail.com>
2018-02-26 14:03:57 -0500

    finished playing with digits challenge

---
+++ solutionsC/funnyDigits/funnyDigits/funnyDigits/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  funnyDigits
+//
+//  Created by NATHAN J MALLISON on 2/24/18.
+//  Copyright © 2018 NATHAN J MALLISON. All rights reserved.
+//
+
+#include <stdio.h>
+#include "funnyDigits.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("my number %d\n", digPow(3456789, 5));
+    printf("Expected: %d\n", -1);
+    return 0;
+}
