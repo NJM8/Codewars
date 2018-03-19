d1f8149be32bd5061e5d30aa38b3a0002785bb1d
Nate Mallison <healthynate@gmail.com>
2018-03-19 08:43:06 -0400

    refactored string repeat challenege based on answers to my question on codewars. first solution did not pass some test.

--- solutionsC/stringRepeat/stringRepeat/stringRepeat/stringRepeat.c
+++ solutionsC/stringRepeat/stringRepeat/stringRepeat/stringRepeat.c
@@ -6,10 +6,19 @@
 //  Copyright © 2018 NATHAN J MALLISON. All rights reserved.
 //
 // This solution works localy but fails on codewars, appears to be very similar to many other solutions.
+// Answer to my question on codewars:
+//
+//uzimonkey(6 kyu)
+//10 hours ago
+//
+//sizeof(src) doesn't do what you think it does, it's returning the size of the pointer type. You want strlen(src) instead.
+//
+//Also, malloc isn't required to return initialized memory. It might have junk data in it, if there is junk data then strcat will do strange things and possibly segfault. You should use calloc instead, which returns 0-initialized memory.
+
 #include "stringRepeat.h"
 
 char *repeat_str(size_t count, const char *src) {
-    char *repeated = malloc(sizeof(src) * count + 1);
+    char *repeated = calloc(strlen(src) * count + 1, strlen(src));
     for (int i = 0; i < count; i++) {
         strcat(repeated, src);
     }
